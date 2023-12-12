c2199534355881348bef69ca6ec4d57298afea1d
Felipe Daiello <felipedaiello@gmail.com>
2023-12-12 17:09:27 -0300

    project setup

---
+++ server/config.js
@@ -0,0 +1,13 @@
+const dotenv = require("dotenv")
+dotenv.config()
+
+module.exports = {
+  twilio: {
+    accountSid: process.env.TWILIO_ACCOUNT_SID,
+    apiKey: process.env.TWILIO_API_KEY,
+    apiSecret: process.env.TWILIO_API_SECRET,
+    outgoingApplicationSid: process.env.TWILIO_TWIML_APP_SID,
+    incomingAllow: process.env.TWILIO_ALLOW_INCOMING_CALLS === "true",
+    callerId: process.env.FROM_NUMBER
+  }
+};
